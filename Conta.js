5411da3d2d3aeb77c6afe90cf6b1492b86cef3cc
Wesley Amaral <wesleyamarals@yahoo.com.br>
2021-10-26 22:11:32 -0300

    first commit

---
+++ Conta.js
@@ -0,0 +1,12 @@
+export class Conta{
+
+    agencia;
+    numero;
+    cliente;
+
+    constructor(agencia,numero,cliente){
+        this.agencia = agencia;
+        this.numero = numero;
+        this.cliente = cliente;
+    }
+}
